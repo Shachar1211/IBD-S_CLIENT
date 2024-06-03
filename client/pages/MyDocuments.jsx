d8ae5639e60da4818fbf336cfada6e4447e9d602
Shachar1211 <manor.shachar@gmail.com>
2024-06-03 17:39:33 +0300

    3

--- client/pages/MyDocuments.jsx
+++ client/pages/MyDocuments.jsx
@@ -1,19 +1,48 @@
 import React, { useState, useCallback } from 'react';
-import { StyleSheet, View, Text } from 'react-native';
+import { StyleSheet, View, Text, TextInput } from 'react-native';
 import AppFooter from '../components/Footer';
 import AppHeader from '../components/Header';
 import { useUser } from '../components/UserContext';
 import Visitor from '../components/visitor';
 import UserAvatar from '../components/avatar';
 import { TouchableOpacity } from 'react-native-gesture-handler';
+import { useFocusEffect } from '@react-navigation/native';
+import AppButton from '../components/buttons';
 
 export default function MyDocuments({ navigation }) {
   const { visitor, imagePaths } = useUser();
   const [FolderExist, setFolderExist] = useState(false);
+  const [newFolderAdded, setnewFolderAdded] = useState(false);
+  const [newFolderSaved, setnewFolderSaved] = useState(false);
   const [FilesExist, setFilesExist] = useState(false);
-  const [folders, setFolders] = useState([{ name: "aaaaaa" }, { name: "ddd" }, { name: "ddd" }]);
+  const [folderName, setfolderName] = useState('שם התיקייה');
+  const [folders, setFolders] = useState([]);
 
-  const separateFoldersRow = useCallback(() => { // הוספת התיקיות
+  function saveFolder(){
+    setFolders(folders.slice(0, -1));
+    var newFolder = { name: folderName };
+    var updateFoldersArr = [newFolder];
+    setFolders(updateFoldersArr);
+    setnewFolderSaved(true);
+    console.log('folders', folders);
+  }
+
+  const showNewFolder = () => {
+    setFolderExist(true);
+    setnewFolderAdded(true);
+    var newFolder = { name: folderName };
+    var updateFolderArr = [...folders, newFolder];
+    setFolders(updateFolderArr);
+  };
+
+  useFocusEffect(
+    useCallback(() => {
+      console.log('folderName', folderName);
+      console.log('folders', folders);
+    }, [folderName])
+  );
+
+  const separateFoldersRow = useCallback(() => {
     const rows = [];
     let oneRow = [];
 
@@ -27,7 +56,16 @@ export default function MyDocuments({ navigation }) {
           <TouchableOpacity>
             <UserAvatar marginTop={30} size={100} source={imagePaths['folder']} />
           </TouchableOpacity>
-          <Text style={styles.folderLabel}>{folders[i].name}</Text>
+          {newFolderAdded && i === folders.length - 1 ? (
+            <TextInput
+              value={folderName}
+              onChangeText={(text) => setfolderName(text)}
+              style={folderName === 'שם התיקייה' ? styles.inputText : styles.FilledinputText}
+              autoFocus={true}
+            />
+          ) : (
+            <Text style={styles.folderLabel}>{folders[i].name}</Text>
+          )}
         </View>
       );
     }
@@ -41,36 +79,43 @@ export default function MyDocuments({ navigation }) {
         {row}
       </View>
     ));
-  }, [folders, imagePaths]);
+  }, [folders, imagePaths, folderName, newFolderAdded]);
 
   return (
-    <View style={[styles.container,!FolderExist && !FilesExist ? { alignItems: 'center' } : null]}>
-      <AppHeader navigation={navigation} backArrow={false} label='מסמכים אישיים' startIcon={true} icon={imagePaths['documentsFill']} />
-      {visitor && <Visitor navigation={navigation} />}
-      {!visitor && (
-        <View>
-          {!FolderExist && !FilesExist && (
-            <View>
-              <Text style={styles.label}>עדיין לא נפתחו תיקיות או הועלו מסמכים</Text>
-              <View style={styles.twoInRowAvatar}>
-                <TouchableOpacity>
-                  <UserAvatar marginRight={60} size={100} source={imagePaths['document']} />
-                </TouchableOpacity>
-                <TouchableOpacity onPress={() => setFolderExist(true)}>
-                  <UserAvatar size={100} source={imagePaths['folder']} />
-                </TouchableOpacity>
-              </View>
-              <View style={styles.twoInRowLabels}>
-                <Text style={styles.addLabel0}>הוספת מסמך</Text>
-                <Text style={styles.addLabel1}>הוספת תיקייה</Text>
-              </View>
+    <View style={[styles.container, !FolderExist && !FilesExist ? { alignItems: 'center' } : null]}>
+      <AppHeader
+        navigation={navigation}
+        backArrow={false}
+        label='מסמכים אישיים'
+        startIcon={true}
+        icon={imagePaths['documentsFill']}
+      />
+      <View>
+        {!FolderExist && !FilesExist && (
+          <View>
+            <Text style={styles.label}>עדיין לא נפתחו תיקיות או הועלו מסמכים</Text>
+            <View style={styles.twoInRowAvatar}>
+              <TouchableOpacity>
+                <UserAvatar marginRight={60} size={100} source={imagePaths['document']} />
+              </TouchableOpacity>
+              <TouchableOpacity onPress={() => showNewFolder()}>
+                <UserAvatar size={100} source={imagePaths['folder']} />
+              </TouchableOpacity>
             </View>
-          )}
-          <View style={styles.folderWrapperContainer}>
-            {FolderExist && folders && separateFoldersRow()}
+            <View style={styles.twoInRowLabels}>
+              <Text style={styles.addLabel0}>הוספת מסמך</Text>
+              <Text style={styles.addLabel1}>הוספת תיקייה</Text>
+            </View>
           </View>
+        )}
+        <View style={styles.folderWrapperContainer}>
+          {FolderExist && folders && separateFoldersRow()}
         </View>
-      )}
+        {FolderExist && folders && !newFolderSaved &&<View style={styles.twoInRowButtons}>
+          <AppButton width={100} bottom={90} borderColor='#9F0405' backgroundColor='#9F0405' label='ביטול' onPressHandler={() => {setFolderExist(false),setFolders([],setfolderName('שם התיקייה'))}}></AppButton>
+          <AppButton width={100} bottom={90} label='שמירה' onPressHandler={() => saveFolder()}></AppButton>
+        </View>} 
+      </View>
       <AppFooter navigation={navigation} />
     </View>
   );
@@ -78,7 +123,6 @@ export default function MyDocuments({ navigation }) {
 
 const styles = StyleSheet.create({
   container: {
-    //alignItems: 'center',
     flexGrow: 1,
     position: 'relative',
     backgroundColor: 'white',
@@ -95,31 +139,39 @@ const styles = StyleSheet.create({
     flexDirection: 'row',
   },
   folderWrapperContainer: {
-    alignItems: 'flex-end', // Align items to the end to achieve right alignment
+    alignItems: 'flex-end',
     width: '100%',
-    position:'relative',
-    //right:150,
-    //left:120,
+    position: 'relative',
   },
   folderContainer: {
     flexDirection: 'row-reverse',
     justifyContent: 'flex-start',
     width: '100%',
     padding: 10,
-    marginRight:30,
+    marginRight: 30,
   },
   folderWrapper: {
     alignItems: 'center',
     marginLeft: 30,
-  },
-  twoInRowLabels: {
-    flexDirection: 'row',
   },
   folderLabel: {
     textAlign: 'center',
     width: 70,
     marginTop: 10,
     color: '#50436E',
+  },
+  inputText: {
+    textAlign: 'center',
+    width: 80,
+    marginTop: 10,
+    backgroundColor: '#D8F3F9',
+    height: 17,
+  },
+  FilledinputText: {
+    textAlign: 'center',
+    width: 80,
+    marginTop: 10,
+    height: 17,
   },
   addLabel0: {
     marginTop: 10,
@@ -135,5 +187,14 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     color: '#50436E',
   },
+  twoInRowLabels: {
+    flexDirection: 'row',
+  },
+  twoInRowButtons:{
+    flexDirection: 'row-reverse',
+    alignItems: 'center',
+    justifyContent: 'space-around',
+    position:'relative',
+    top:520,
+  },
 });
-
